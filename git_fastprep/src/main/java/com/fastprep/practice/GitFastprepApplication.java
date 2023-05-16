c35c1d679c23accac6e28178395a9f80f47a97f4
Manish Chauhan <manish.fastprep@gmail.com>
2023-05-16 16:13:09 +0530

    staged changes committed

---
+++ git_fastprep/src/main/java/com/fastprep/practice/GitFastprepApplication.java
@@ -0,0 +1,13 @@
+package com.fastprep.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFastprepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFastprepApplication.class, args);
+	}
+
+}
