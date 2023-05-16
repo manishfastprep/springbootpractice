824f53388f12b4df66ea1093de026098e90f9fab
Manish Chauhan <manish.fastprep@gmail.com>
2023-05-16 09:07:23 +0530

    1. Created a new project . 2. Removed redundant run configurations.

---
+++ git_fastprep/src/test/java/com/fastprep/practice/GitFastprepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastprep.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFastprepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
